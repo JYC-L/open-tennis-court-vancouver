bef485ed2527895e44864e6f5b517bae23b82e1c
Liu, Tong <tliu024@student.ubc.ca>
2025-06-20 19:42:18 -0700

    deleted dist

--- backend/dist/routes/sampleRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.setSampleRoutes = void 0;
-const sampleController_1 = require("../controllers/sampleController");
-function setSampleRoutes(app) {
-    const controller = new sampleController_1.SampleController();
-    app.get('/api/sample', controller.getSampleData.bind(controller));
-}
-exports.setSampleRoutes = setSampleRoutes;
