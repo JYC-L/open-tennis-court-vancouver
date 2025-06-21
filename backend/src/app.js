3b886dacd97769a2b6ef37f090cb50711b26c088
Francis Huang <yhuan110@students.cs.ubc.ca>
2025-06-20 22:41:55 -0700

    Dockeried

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/availability", availabilityRoutes);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
